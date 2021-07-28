11cd3d070ba11f5d974f8d7a01e979db38f7167d
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-07-28 15:47:07 -0300

    Adicionado configuraçao bootstrap. Adicionado botoes para avançar imagems.

--- src/componets/BestSellers.jsx
+++ src/componets/BestSellers.jsx
@@ -4,24 +4,29 @@ import '../style/bestSellers.css';
 
   function BestSellers() {
     const { productsApi, setProductsCart } = useContext(UserContext);
+    // para as avaliaçõe em estrelas fazer um icone e renderiza-lo de acorod com a quantidade. Google foonts ou bootstrap
     function cardItemCreator(productsApi) {
       return (
-        productsApi.map((product) => (
-          <div key={product.productId}>
-            <img alt={`foto ${product.productName}`} src={product.imageUrl} />
-            <div>
-              <p>{product.productName}</p>
-              <p>{`${product.stars} estrelas`}</p>
-              <p>{`Valor: R$${Number(product.price)}`}</p>
-              <p>Valor parcelado - fazer calculo simulado</p>
-              <input
-                type="button"
-                value="Comprar"
-                onClick={ () => setProductsCart(product) }
-              />
+        <div className="list-BestSellers">
+          <input type="button" value="<"/>
+          {productsApi.map((product) => (
+            <div className="item-BestSellers container-fluid"key={product.productId}>
+              <img alt={`foto ${product.productName}`} src={product.imageUrl} />
+              <div className="infos-item">
+                <p className="product-name">{product.productName}</p>
+                <p>{`${product.stars} estrelas`}</p>
+                <p>{`Valor: R$${Number(product.price)}`}</p>
+                <p>Valor parcelado - fazer calculo simulado</p>
+                <input
+                  type="button"
+                  value="Comprar"
+                  onClick={ () => setProductsCart(product) }
+                />
+              </div>
             </div>
-          </div>
-        )) 
+          ))} 
+          <input type="button" value=">"/>
+        </div>
       )
     }
   return (
