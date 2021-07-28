67187f321238d44fbe2345bc3fd42763ac8c06b0
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-07-28 17:23:10 -0300

    Adicionado logica de passagem de itens da vitrine. Corrigir bug de ordem na renderizaçao.

--- src/componets/BestSellers.jsx
+++ src/componets/BestSellers.jsx
@@ -1,31 +1,56 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import UserContext from '../context/UserContext';
 import '../style/bestSellers.css';
 
   function BestSellers() {
+    // const [indexLimit, setIndexLimit] = useState(3);
+    const [indexToNotRender, setIndexToNotRender] = useState(4);
     const { productsApi, setProductsCart } = useContext(UserContext);
     // para as avaliaçõe em estrelas fazer um icone e renderiza-lo de acorod com a quantidade. Google foonts ou bootstrap
+
+    function handleRenderBestSellers(operation) {
+      if (operation === '>') {
+        (indexToNotRender === 4) ? setIndexToNotRender(0) : setIndexToNotRender(indexToNotRender + 1);
+      }
+      if (operation === '<') {
+        (indexToNotRender === 0) ? setIndexToNotRender(4) : setIndexToNotRender(indexToNotRender - 1);
+      }
+    }
+
     function cardItemCreator(productsApi) {
+      // Faço um arrei com todos os item a serem renderizados.
+      const cartList = productsApi.map((product, index) => (
+        (index === indexToNotRender) ? null : ( <div
+          className="item-BestSellers container-fluid"
+          key={product.productId}
+        >
+          <img alt={`foto ${product.productName}`} src={product.imageUrl} />
+          <div className="infos-item">
+            <p className="product-name">{product.productName}</p>
+            <p>{`${product.stars} estrelas`}</p>
+            <p>{`Valor: R$${Number(product.price)}`}</p>
+            <p>Valor parcelado - fazer calculo simulado</p>
+            <input
+              type="button"
+              value="Comprar"
+              onClick={ () => setProductsCart(product) }
+            />
+          </div>
+        </div>)
+      ))
       return (
         <div className="list-BestSellers">
-          <input type="button" value="<"/>
-          {productsApi.map((product) => (
-            <div className="item-BestSellers container-fluid"key={product.productId}>
-              <img alt={`foto ${product.productName}`} src={product.imageUrl} />
-              <div className="infos-item">
-                <p className="product-name">{product.productName}</p>
-                <p>{`${product.stars} estrelas`}</p>
-                <p>{`Valor: R$${Number(product.price)}`}</p>
-                <p>Valor parcelado - fazer calculo simulado</p>
-                <input
-                  type="button"
-                  value="Comprar"
-                  onClick={ () => setProductsCart(product) }
-                />
-              </div>
-            </div>
-          ))} 
-          <input type="button" value=">"/>
+          <input
+            type="button"
+            value="<"
+            onClick={({target: { value }}) => handleRenderBestSellers(value) }
+          />
+          {cartList} 
+          <input
+            type="button"
+            value=">"
+            onClick={({target: { value }}) => handleRenderBestSellers(value) }
+          />
         </div>
       )
     }
